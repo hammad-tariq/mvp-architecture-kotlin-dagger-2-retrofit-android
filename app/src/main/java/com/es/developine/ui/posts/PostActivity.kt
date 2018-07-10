5702f526175dc15ec7d955ef382ca7b422223ab1
ehtisham.haq <ehtisham.haq@evampsaanga.com>
2018-07-10 15:51:02 +0500

    PostAdapter location changed

--- app/src/main/java/com/es/developine/ui/posts/PostActivity.kt
+++ app/src/main/java/com/es/developine/ui/posts/PostActivity.kt
@@ -1,16 +1,12 @@
 package com.es.developine.ui.posts
 
-import android.app.Application
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
-import com.es.developine.ApplicationClass
 import com.es.developine.R
 import com.es.developine.data.PostData
 import com.es.developine.ui.BaseActivity
-import com.es.developine.ui.login.LoginPresenter
-import com.es.developine.ui.login.LoginPresenterImpl
+import com.es.developine.ui.adapters.PostItemAdapter
 import kotlinx.android.synthetic.main.activity_post.*
 
 class PostActivity : BaseActivity(), PostView {
